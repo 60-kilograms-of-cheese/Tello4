d738c7c32dc1e215fa7d96d4234002f78ad79a6f
60-kilograms-of-cheese <105305076+60-kilograms-of-cheese@users.noreply.github.com>
2022-05-12 09:53:27 -0700

    Add files via upload

---
+++ droan.py
@@ -0,0 +1,77 @@
+
+#flips don't work right now
+
+from djitellopy import tello
+from time import sleep
+
+tello = tello.Tello()
+tello.connect
+
+#launch
+
+tello.takeoff()
+sleep(1)
+
+#move up 90cm
+
+tello.move_up(90)
+sleep(1)
+
+#flip forward
+
+#tello.flip_forward()
+#sleep(1)
+
+#move forward 800cm
+
+tello.move_forward(800)
+sleep(1)
+
+#flip backward
+
+#tello.flip_back()
+#sleep(1)
+
+#turn left 90
+
+tello.rotate_counter_clockwise(90)
+sleep(1)
+
+#move forward 400cm
+
+tello.move_forward(400)
+sleep(1)
+
+#flip left
+
+#tello.flip_left()
+#sleep(1)
+
+#turn left 90
+
+tello.rotate_counter_clockwise(90)
+sleep(1)
+
+#move forward 800cm
+
+tello.move_forward(800)
+sleep(1)
+
+#flip right
+
+#tello.flip_right()
+#sleep(1)
+
+#turn left 90
+
+tello.rotate_counter_clockwise(90)
+sleep(1)
+
+#move forward 400cm
+
+tello.move_forward(400)
+sleep(1)
+
+#land
+
+tello.land()
